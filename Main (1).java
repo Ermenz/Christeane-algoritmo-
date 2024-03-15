063f9a5887d86bf5fd460afb9fcc24298cde538a
Ermenz <143419782+Ermenz@users.noreply.github.com>
2024-03-15 20:19:28 -0300

    Add files via upload

---
+++ Main (1).java
@@ -0,0 +1,48 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+  GDB online is an online compiler and debugger tool for C, C++, Python, PHP, Ruby, 
+  C#, OCaml, VB, Perl, Swift, Prolog, Javascript, Pascal, COBOL, HTML, CSS, JS
+  Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("É um novo cliente ?");
+        String condicao1 = sc.next();
+       
+        
+        System.out.println("\n");
+
+
+
+        if (condicao1.equals("nao")) {
+        System.out.println("O cliente fez mais de 5 compras nos últimos 6 meses ?");
+        String condicao2 = sc.next();
+        }
+        
+        
+        
+        System.out.println("\n");
+
+        System.out.println("Informe o valor total das compras"); 
+        float valorCompras = sc.nextFloat();
+
+        System.out.println("\n");
+
+        if(condicao1.equals("sim") && valorCompras > 500) {
+            System.out.println("Não podem fazer compras com valor total superior a esse limite até que completem 3 meses como clientes ativos.");   
+        } else if (condicao1.equals("sim") && valorCompras < 500) {
+            System.out.println(String.format("Valor das compras: %.2f", valorCompras));  
+        } else if  (condicao1.equals("sim") && condicao2.equals("sim")){
+            float valorComDesconto = valorCompras - valorCompras * 0.1f;
+            System.out.println(String.format("Valor das compras com desconto de 10%%: %.2f", valorComDesconto));  
+        } else if  (condicao1.equals("sim") && condicao2.equals("não")){
+            System.out.println(String.format("Valor das compras com desconto de 10%%: %.2f", valorCompras));  
+        }
+    }
+}
